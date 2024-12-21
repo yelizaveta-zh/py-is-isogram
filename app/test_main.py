e1e1af94aaf101f59c0d742f2aa073570fef7f57
Lisa <zhyganiuk.elizaveta@gmail.com>
2024-12-21 18:39:19 +0200

    solution_add_parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -2,17 +2,15 @@ from app.main import is_isogram
 import pytest
 
 
-def test_empty_string_is_isogram() -> None:
-    assert is_isogram('') is True
-
-
-def test_isogram_with_unique_letters() -> None:
-    assert is_isogram('playgrounds') is True
-
-
-def test_isogram_with_repeated_letters() -> None:
-    assert is_isogram('look') is False
-
-
-def test_isogram_with_case_insensitivity() -> None:
-    assert is_isogram('Adam') is False
+@pytest.mark.parametrize(
+    "word, expected",
+    [
+        ("", True),
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+    ],
+)
+def test_is_isogram(word: str, expected: bool) -> None:
+    assert is_isogram(word) == expected
+    assert isinstance(is_isogram(word), bool)
